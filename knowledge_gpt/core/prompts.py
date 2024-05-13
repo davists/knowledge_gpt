08ce071f31119d2b3492787a78084a53ada84b74
davi dos santos <davi646@gmail.com>
2024-05-13 20:54:39 +0000

    elaboracao documento

--- knowledge_gpt/core/prompts.py
+++ knowledge_gpt/core/prompts.py
@@ -1,37 +1,16 @@
 # flake8: noqa
 from langchain.prompts import PromptTemplate
 
-## Use a shorter template to reduce the number of tokens in the prompt
-template = """Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer.
-
----------
-
-QUESTION: What  is the purpose of ARPA-H?
-=========
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt's based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer's, diabetes, and more.
-SOURCES: 1-32
-Content: While we're at it, let's make sure every American can get the health care they need. \n\nWe've already made historic investments in health care. \n\nWe've made it easier for Americans to get the care they need, when they need it. \n\nWe've made it easier for Americans to get the treatments they need, when they need them. \n\nWe've made it easier for Americans to get the medications they need, when they need them.
-SOURCES: 1-33
-Content: The V.A. is pioneering new ways of linking toxic exposures to disease, already helping  veterans get the care they deserve. \n\nWe need to extend that same care to all Americans. \n\nThat's why I'm calling on Congress to pass legislation that would establish a national registry of toxic exposures, and provide health care and financial assistance to those affected.
-SOURCES: 1-30
-=========
-FINAL ANSWER: The purpose of ARPA-H is to drive breakthroughs in cancer, Alzheimer's, diabetes, and more.
-SOURCES: 1-32
-
----------
+# Use a shorter template to reduce the number of tokens in the prompt
+template = """Gere uma minuta de escritura pública de compra e venda use como referencia o _document_ preencha os dados pessoais com as informações em _dados_basicos_.
+Crie uma resposta final em português retornando apenas o documento gerado com texto formatado para melhor visualização.
 
 QUESTION: {question}
 =========
 {summaries}
 =========
-FINAL ANSWER:"""
-
-
-# template = """
-#     Responda as perguntas com base no context: 
-#     <context>{context}</context>.
-#     Caso não consiga responder baseado no context, informar que a informação não está registrada na base de conhecimento. Sempre responda em português.
-# """
+FINAL ANSWER:
+"""
 
 
 STUFF_PROMPT = PromptTemplate(
